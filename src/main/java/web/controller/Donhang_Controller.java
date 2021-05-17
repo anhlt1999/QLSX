69a828ef89ef2a06ef1c85f7e8c6f5c02d883fcb
leeanh99 <anhlt.b17cn020@stu.ptit.edu.vn>
2021-05-17 17:09:56 +0700

    Signed-off-by: leeanh99 <anhlt.b17cn020@stu.ptit.edu.vn>

--- src/main/java/web/controller/Donhang_Controller.java
+++ src/main/java/web/controller/Donhang_Controller.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import web.entity.Donhang;
 import web.entity.NVL;
@@ -12,8 +15,7 @@ import web.repo.DonhangRepository;
 
 import java.util.List;
 
-@RestController
-@CrossOrigin(origins = "https://qlsanxuat.herokuapp.com")
+@Controller
 @RequestMapping("/donhang")
 public class Donhang_Controller {
 	private final DonhangRepository donhangRepo;
@@ -22,31 +24,38 @@ public class Donhang_Controller {
 	public Donhang_Controller(DonhangRepository donhangRepo) {
 		this.donhangRepo = donhangRepo;
 	}
+	
 	@GetMapping("/getAll")
-	public List<Donhang> getAll(){
-		return (List<Donhang>) donhangRepo.findAll();
+	public String getAll(Model model){
+		model.addAttribute("donhang",donhangRepo.findAll());
+		return "qldh/donhang";
 	}
+	
 	@GetMapping("/add")
 	public String addDonhang(Model model){
 		model.addAttribute("donhang",new Donhang());
-		return "formDonhang";
+		return "qldh/formDonhang";
 	}
+	
 	@GetMapping("/edit/{id}")
 	public String editDonhang(Model model, @PathVariable Long id){
 		model.addAttribute("donhang",donhangRepo.findById(id).get());
-		return "formDonhang";
+		return "qldh/formDonhang";
 	}
+	
 	@GetMapping("/delete/{id}")
 	public String deleteDh(Model model, @PathVariable Long id){
 		donhangRepo.deleteById(id);
 		return "redirect:/donhang/getAll";
 	}
+	
 	@GetMapping("/search")
 	public String searchDonhang(@Param("keyword") String keyword, Model model) {
-			List<Donhang> list = donhangRepo.findAllByMotaContaining(keyword);
+			List<Donhang> list = (List<Donhang>) donhangRepo.findAllByMotaContaining(keyword);
 			model.addAttribute("donhang", list);
-		return "donhang";
+		return "qldh/donhang";
 	}
+	
 	@PostMapping("/save")
 	public String addDonhang(Donhang dh){
 		donhangRepo.save(dh);
